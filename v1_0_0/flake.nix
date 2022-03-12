ea13d674d1a55e056d6d13de12c12e3c0863e2e5
hugosenari <hugosenari@gmail.com>
2022-03-12 00:42:31 -0300

    chore: re index ballpark

---
+++ v1_0_0/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''An amateur radio tool to get you a ballpark estimate of where a given Maidenhead grid square is.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-ballpark-v1_0_0.flake = false;
+  inputs.src-ballpark-v1_0_0.owner = "Mihara";
+  inputs.src-ballpark-v1_0_0.ref   = "refs/tags/v1.0.0";
+  inputs.src-ballpark-v1_0_0.repo  = "ballpark";
+  inputs.src-ballpark-v1_0_0.type  = "github";
+  
+  inputs."fsnotify".dir   = "nimpkgs/f/fsnotify";
+  inputs."fsnotify".owner = "riinr";
+  inputs."fsnotify".ref   = "flake-pinning";
+  inputs."fsnotify".repo  = "flake-nimble";
+  inputs."fsnotify".type  = "github";
+  inputs."fsnotify".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-ballpark-v1_0_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-ballpark-v1_0_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
